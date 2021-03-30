3824a6ea5fde4c200b2c25dd94bb541331771463
Dan Williams <dan@331.io>
2021-03-30 09:27:35 -0400

    Update confirm.go

--- internal/dragonchain/confirm.go
+++ internal/dragonchain/confirm.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/dragonchain/dragonchain-installer/internal/configuration"
+	"github.com/deftOfCenter/dragonchain-installer/internal/configuration"
 )
 
 type kubectlPodJSONList struct {
