5c32d3f268d359e21e96722cdcf73b337ce83aa2
Dan Williams <dan@331.io>
2021-03-30 09:28:20 -0400

    Update registry.go

--- internal/dragonchain/registry.go
+++ internal/dragonchain/registry.go
@@ -7,7 +7,7 @@ import (
 	"os/exec"
 	"strconv"
 
-	"github.com/dragonchain/dragonchain-installer/internal/configuration"
+	"github.com/deftOfCenter/dragonchain-installer/internal/configuration"
 )
 
 var registryNamespacesYaml = []byte(`
